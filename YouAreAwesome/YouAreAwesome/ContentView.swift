aba1a66f94ac017677c3e033e3727a671fcffdcc
lmsutton31 <lmsutton31@gmail.com>
2026-01-19 16:15:39 -0500

    Add files via upload

---
+++ YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Luke Sutton on 1/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius:30))
+                .shadow(radius: 30)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            Spacer()
+            
+            Button("Show Message") {
+                let message1 = "You Are Awesome!"
+                let message2 = "You Are Great!"
+                
+                message = (message == message1 ? message2 : message1 )
+                //                imageName = (imageName == "image0" ? "image2" : "image0" )
+
+                imageName = "image\(imageNumber)"
+                //                imageNumber = imageNumber + 1
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
