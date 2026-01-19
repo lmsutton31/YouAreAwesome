73ab9a11b4733a1599e927b628139c97b4856b91
Luke Sutton <lmsutton31@gmai.com>
2026-01-19 15:22:38 -0500

    Update after 1.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -31,13 +31,16 @@ struct ContentView: View {
                 let imageString1 = "sun.max.fill"
                 let imageString2 = "hand.thumbsup"
                 
-                if message == message1 {
-                    message = message2
-                    imageString = imageString2
-                } else {
-                    message = message1
-                    imageString = imageString1
-                }
+//                if message == message1 {
+//                    message = message2
+//                    imageString = imageString2
+//                } else {
+//                    message = message1
+//                    imageString = imageString1
+//                }
+                
+                message = (message == message1 ? message2 : message1 )
+                imageString = (imageString == imageString1 ? imageString2 : imageString1 )
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
