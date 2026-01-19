e217c6ce3ff0cb679ec42845a0379c06324e4b71
Luke Sutton <lmsutton31@gmai.com>
2026-01-19 16:10:49 -0500

    After 1.8

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
     var body: some View {
         
         VStack {
@@ -25,12 +26,20 @@ struct ContentView: View {
                 .foregroundStyle(.red)
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message") {
                 let message1 = "You Are Awesome!"
                 let message2 = "You Are Great!"
-
+                
                 message = (message == message1 ? message2 : message1 )
-                imageName = (imageName == "image0" ? "image2" : "image0" )
+                //                imageName = (imageName == "image0" ? "image2" : "image0" )
+
+                imageName = "image\(imageNumber)"
+                //                imageNumber = imageNumber + 1
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
